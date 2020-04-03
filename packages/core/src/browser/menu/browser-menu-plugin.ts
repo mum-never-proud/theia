a091306300485591a547259b989c20c62fbc189d
Akos Kitta <kittaakos@typefox.io>
2020-04-03 14:01:24 +0200

    Calculate the menu enablement before showing it.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- packages/core/src/browser/menu/browser-menu-plugin.ts
+++ packages/core/src/browser/menu/browser-menu-plugin.ts
@@ -14,14 +14,12 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { injectable, inject } from 'inversify';
 import { MenuBar, Menu as MenuWidget, Widget } from '@phosphor/widgets';
 import { CommandRegistry as PhosphorCommandRegistry } from '@phosphor/commands';
 import {
     CommandRegistry, ActionMenuNode, CompositeMenuNode,
-    MenuModelRegistry, MAIN_MENU_BAR, MenuPath, ILogger
+    MenuModelRegistry, MAIN_MENU_BAR, MenuPath, DisposableCollection, Disposable
 } from '../../common';
 import { KeybindingRegistry } from '../keybinding';
 import { FrontendApplicationContribution, FrontendApplication } from '../frontend-application';
@@ -38,20 +36,20 @@ export abstract class MenuBarWidget extends MenuBar {
 @injectable()
 export class BrowserMainMenuFactory {
 
-    @inject(ILogger)
-    protected readonly logger: ILogger;
-
     @inject(ContextKeyService)
     protected readonly contextKeyService: ContextKeyService;
 
     @inject(ContextMenuContext)
     protected readonly context: ContextMenuContext;
 
-    constructor(
-        @inject(CommandRegistry) protected readonly commandRegistry: CommandRegistry,
-        @inject(KeybindingRegistry) protected readonly keybindingRegistry: KeybindingRegistry,
-        @inject(MenuModelRegistry) protected readonly menuProvider: MenuModelRegistry
-    ) { }
+    @inject(CommandRegistry)
+    protected readonly commandRegistry: CommandRegistry;
+
+    @inject(KeybindingRegistry)
+    protected readonly keybindingRegistry: KeybindingRegistry;
+
+    @inject(MenuModelRegistry)
+    protected readonly menuProvider: MenuModelRegistry;
 
     createMenuBar(): MenuBarWidget {
         const menuBar = new DynamicMenuBarWidget();
@@ -67,76 +65,53 @@ export class BrowserMainMenuFactory {
 
     protected fillMenuBar(menuBar: MenuBarWidget): void {
         const menuModel = this.menuProvider.getMenu(MAIN_MENU_BAR);
-        const phosphorCommands = this.createPhosphorCommands(menuModel);
-        // for the main menu we want all items to be visible.
-        phosphorCommands.isVisible = () => true;
-
+        const menuCommandRegistry = this.createMenuCommandRegistry(menuModel);
         for (const menu of menuModel.children) {
             if (menu instanceof CompositeMenuNode) {
-                const menuWidget = new DynamicMenuWidget(menu, { commands: phosphorCommands }, this.contextKeyService, this.context);
+                const menuWidget = new DynamicMenuWidget(menu, { commands: menuCommandRegistry }, this.services);
                 menuBar.addMenu(menuWidget);
             }
         }
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     createContextMenu(path: MenuPath, args?: any[]): MenuWidget {
         const menuModel = this.menuProvider.getMenu(path);
-        const phosphorCommands = this.createPhosphorCommands(menuModel, args);
-
-        const contextMenu = new DynamicMenuWidget(menuModel, { commands: phosphorCommands }, this.contextKeyService, this.context);
+        const menuCommandRegistry = this.createMenuCommandRegistry(menuModel, args).use();
+        const contextMenu = new DynamicMenuWidget(menuModel, { commands: menuCommandRegistry }, this.services);
         return contextMenu;
     }
 
-    protected createPhosphorCommands(menu: CompositeMenuNode, args: any[] = []): PhosphorCommandRegistry {
-        const commands = new PhosphorCommandRegistry();
-        this.addPhosphorCommands(commands, menu, args);
-        return commands;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    protected createMenuCommandRegistry(menu: CompositeMenuNode, args: any[] = []): MenuCommandRegistry {
+        const menuCommandRegistry = new MenuCommandRegistry(this.services);
+        this.registerMenu(menuCommandRegistry, menu, args);
+        return menuCommandRegistry;
     }
 
-    protected addPhosphorCommands(commands: PhosphorCommandRegistry, menu: CompositeMenuNode, args: any[]): void {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    protected registerMenu(menuCommandRegistry: MenuCommandRegistry, menu: CompositeMenuNode, args: any[]): void {
         for (const child of menu.children) {
             if (child instanceof ActionMenuNode) {
-                this.addPhosphorCommand(commands, child, args);
+                menuCommandRegistry.registerActionMenu(child, args);
                 if (child.altNode) {
-                    this.addPhosphorCommand(commands, child.altNode, args);
+                    menuCommandRegistry.registerActionMenu(child.altNode, args);
                 }
             } else if (child instanceof CompositeMenuNode) {
-                this.addPhosphorCommands(commands, child, args);
+                this.registerMenu(menuCommandRegistry, child, args);
             }
         }
     }
 
-    protected addPhosphorCommand(commands: PhosphorCommandRegistry, menu: ActionMenuNode, args: any[]): void {
-        const command = this.commandRegistry.getCommand(menu.action.commandId);
-        if (!command) {
-            return;
-        }
-        if (commands.hasCommand(command.id)) {
-            // several menu items can be registered for the same command in different contexts
-            return;
-        }
-        commands.addCommand(command.id, {
-            execute: () => this.commandRegistry.executeCommand(command.id, ...args),
-            label: menu.label,
-            icon: menu.icon,
-            isEnabled: () => this.commandRegistry.isEnabled(command.id, ...args),
-            isVisible: () => this.commandRegistry.isVisible(command.id, ...args),
-            isToggled: () => this.commandRegistry.isToggled(command.id, ...args)
-        });
-
-        const bindings = this.keybindingRegistry.getKeybindingsForCommand(command.id);
-
-        /* Only consider the first keybinding. */
-        if (bindings.length > 0) {
-            const binding = bindings[0];
-            const keys = this.keybindingRegistry.acceleratorFor(binding);
-            commands.addKeyBinding({
-                command: command.id,
-                keys,
-                selector: '.p-Widget' // We have the Phosphor.JS dependency anyway.
-            });
-        }
+    private get services(): MenuServices {
+        return {
+            context: this.context,
+            contextKeyService: this.contextKeyService,
+            commandRegistry: this.commandRegistry,
+            keybindingRegistry: this.keybindingRegistry
+        };
     }
+
 }
 
 class DynamicMenuBarWidget extends MenuBarWidget {
@@ -193,16 +168,28 @@ class DynamicMenuBarWidget extends MenuBarWidget {
     }
 
 }
+
+class MenuServices {
+    readonly commandRegistry: CommandRegistry;
+    readonly keybindingRegistry: KeybindingRegistry;
+    readonly contextKeyService: ContextKeyService;
+    readonly context: ContextMenuContext;
+}
+
 /**
- * A menu widget that would recompute its items on update
+ * A menu widget that would recompute its items on update.
  */
 class DynamicMenuWidget extends MenuWidget {
 
+    /**
+     * We want to restore the focus after the menu closes.
+     */
+    protected previousFocusedElement?: HTMLElement;
+
     constructor(
         protected menu: CompositeMenuNode,
-        protected options: MenuWidget.IOptions,
-        protected readonly contextKeyService: ContextKeyService,
-        protected readonly context: ContextMenuContext
+        protected options: MenuWidget.IOptions & { commands: MenuCommandRegistry },
+        protected services: MenuServices
     ) {
         super(options);
         if (menu.label) {
@@ -215,79 +202,63 @@ class DynamicMenuWidget extends MenuWidget {
     }
 
     public aboutToShow(): void {
+        this.preserveFocusedElement();
         this.clearItems();
-        this.updateSubMenus(this, this.menu, this.options.commands);
+        this.runWithPreservedFocusElement(() => {
+            this.options.commands.use();
+            this.updateSubMenus(this, this.menu, this.options.commands);
+        });
     }
 
     public open(x: number, y: number, options?: MenuWidget.IOpenOptions): void {
-        // we want to restore the focus after the menu closes.
-        const previouslyActive = window.document.activeElement as HTMLElement;
         const cb = () => {
-            previouslyActive.focus({ preventScroll: true });
+            this.releaseFocusedElement();
             this.aboutToClose.disconnect(cb);
         };
         this.aboutToClose.connect(cb);
         super.open(x, y, options);
     }
 
-    private updateSubMenus(
-        parent: MenuWidget,
-        menu: CompositeMenuNode,
-        commands: PhosphorCommandRegistry
-    ): void {
+    private updateSubMenus(parent: MenuWidget, menu: CompositeMenuNode, commands: MenuCommandRegistry): void {
         const items = this.buildSubMenus([], menu, commands);
         for (const item of items) {
             parent.addItem(item);
         }
     }
 
-    private buildSubMenus(
-        items: MenuWidget.IItemOptions[],
-        menu: CompositeMenuNode,
-        commands: PhosphorCommandRegistry
-    ): MenuWidget.IItemOptions[] {
+    private buildSubMenus(items: MenuWidget.IItemOptions[], menu: CompositeMenuNode, commands: MenuCommandRegistry): MenuWidget.IItemOptions[] {
         for (const item of menu.children) {
             if (item instanceof CompositeMenuNode) {
-                if (item.children.length > 0) {
-                    // do not render empty nodes
-
+                if (item.children.length) { // do not render empty nodes
                     if (item.isSubmenu) { // submenu node
-                        const submenu = new DynamicMenuWidget(item, this.options, this.contextKeyService, this.context);
-                        if (submenu.items.length === 0) {
+                        const submenu = new DynamicMenuWidget(item, this.options, this.services);
+                        if (!submenu.items.length) {
                             continue;
                         }
-
                         items.push({
                             type: 'submenu',
                             submenu,
                         });
-
                     } else { // group node
-
                         const submenu = this.buildSubMenus([], item, commands);
-                        if (submenu.length === 0) {
+                        if (!submenu.length) {
                             continue;
                         }
-
-                        if (items.length > 0) {
-                            // do not put a separator above the first group
-
+                        if (items.length) { // do not put a separator above the first group
                             items.push({
                                 type: 'separator'
                             });
                         }
-
-                        // render children
-                        items.push(...submenu);
+                        items.push(...submenu); // render children
                     }
                 }
             } else if (item instanceof ActionMenuNode) {
-                const node = item.altNode && this.context.altPressed ? item.altNode : item;
+                const { context, contextKeyService } = this.services;
+                const node = item.altNode && context.altPressed ? item.altNode : item;
                 const { when } = node.action;
-                if (!(commands.isVisible(node.action.commandId) && (!when || this.contextKeyService.match(when)))) {
+                if (!(commands.isVisible(node.action.commandId) && (!when || contextKeyService.match(when)))) {
                     continue;
                 }
-
                 items.push({
                     command: node.action.commandId,
                     type: 'command'
@@ -295,6 +266,60 @@ class DynamicMenuWidget extends MenuWidget {
             }
         }
         return items;
+    }
+
+    /**
+     * Preserves the "focused" HTML element (`document.activeElement`) so that we can restore focus right before:
+     *  - executing the underlying command, or
+     *  - when the command has not been executed, but the menu was closed (`Esc`, or click somewhere else).
+     * Returns `true` if the active element has been preserved. Otherwise, `false`. Calling this method after preserving the
+     * focused element is a NOOP.
+     */
+    protected preserveFocusedElement(): boolean {
+        if (!this.previousFocusedElement) {
+            const { activeElement } = window.document;
+            if (activeElement instanceof HTMLElement) {
+                this.previousFocusedElement = activeElement;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Restores the focus on the previously preserved HTML element.
+     * Has no effect, if the focus was already restored, or there was no preserved focused element.
+     * `true` if the restoration was successful. Otherwise, `false`.
+     */
+    protected releaseFocusedElement(): boolean {
+        if (this.previousFocusedElement) {
+            this.previousFocusedElement.focus({ preventScroll: true });
+            this.previousFocusedElement = undefined;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This method executes `what` with the following conditions:
+     *  - Temporarily sets the focus on the `previousFocusedElement` so this context will be used for command handler calculation.
+     *  - Runs `what`. `what` must be executed synchronously.
+     *  - Sets back the focus on the original `activeElement`.
+     */
+    protected runWithPreservedFocusElement(what: () => void): void {
+        let focusToRestore: HTMLElement | undefined = undefined;
+        const { activeElement } = document;
+        if (this.previousFocusedElement && activeElement instanceof HTMLElement && this.previousFocusedElement !== activeElement) {
+            focusToRestore = activeElement;
+            this.previousFocusedElement.focus({ preventScroll: true });
+        }
+        try {
+            what();
+        } finally {
+            if (focusToRestore) {
+                focusToRestore.focus({ preventScroll: true });
+            }
+        }
     }
 
 }
@@ -326,4 +351,85 @@ export class BrowserMenuBarContribution implements FrontendApplicationContributi
         logo.addClass('theia-icon');
         return logo;
     }
+}
+
+/**
+ * Customized PhosphorJS command registry. Instead of registering the commands, we register
+ * menu items. If you want to populate the commands for the menu items, call `use`,
+ * and it will populate the command registry and freeze its state. So the backing command handlers
+ * of the menu items won't change once the menu is visible.
+ */
+class MenuCommandRegistry extends PhosphorCommandRegistry {
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    protected actions = new Map<string, [ActionMenuNode, any[]]>();
+    protected toDisposeOnReset = new DisposableCollection();
+
+    constructor(protected services: MenuServices) {
+        super();
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    registerActionMenu(menu: ActionMenuNode, ...args: any[]): void {
+        const { commandRegistry } = this.services;
+        const command = commandRegistry.getCommand(menu.action.commandId);
+        if (!command) {
+            return;
+        }
+        const { id } = command;
+        if (this.actions.has(id)) {
+            return;
+        }
+        this.actions.set(id, [menu, args]);
+    }
+
+    use(): this {
+        this.toDisposeOnReset.dispose();
+        for (const [menu, args] of this.actions.values()) {
+            this.toDisposeOnReset.push(this.registerCommand(menu, args));
+        }
+        return this;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    protected registerCommand(menu: ActionMenuNode, args: any[]): Disposable {
+        const { commandRegistry, keybindingRegistry } = this.services;
+        const command = commandRegistry.getCommand(menu.action.commandId);
+        if (!command) {
+            return Disposable.NULL;
+        }
+        const { id } = command;
+        if (this.hasCommand(id)) {
+            // several menu items can be registered for the same command in different contexts
+            return Disposable.NULL;
+        }
+
+        // We freeze the `isEnabled`, `isVisible`, and `isToggled` states so they won't change.
+        const enabled = commandRegistry.isEnabled(id, ...args);
+        const visible = commandRegistry.isVisible(id, ...args);
+        const toggled = commandRegistry.isToggled(id, ...args);
+        const unregisterCommand = this.addCommand(id, {
+            execute: () => commandRegistry.executeCommand(id, ...args),
+            label: menu.label,
+            icon: menu.icon,
+            isEnabled: () => enabled,
+            isVisible: () => visible,
+            isToggled: () => toggled
+        });
+
+        const bindings = keybindingRegistry.getKeybindingsForCommand(id);
+
+        // Only consider the first keybinding.
+        if (bindings.length) {
+            const binding = bindings[0];
+            const keys = keybindingRegistry.acceleratorFor(binding);
+            this.addKeyBinding({
+                command: id,
+                keys,
+                selector: '.p-Widget' // We have the PhosphorJS dependency anyway.
+            });
+        }
+        return Disposable.create(() => unregisterCommand.dispose());
+    }
+
 }
